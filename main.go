5d9c3055fd9b377eff5d3ba89d12d0a708da8e0a
dani_edo <dani.edogawa.17@gmail.com>
2025-03-09 16:33:00 +0700

    TaxIncludedPriceJob package (more efficient code)

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"edo.com/price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64) // make: create an empty map (len shouldn't be defined when creating the map)
-
 	for _, taxRate := range taxRates {
-		taxIndludedPrices := make([]float64, len(prices)) // make: create an empty slice (len should be defined when creating the slice)
-		for priceIndex, price := range prices {
-			taxIndludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIndludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 }
