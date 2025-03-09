1a57834f8870c3c6e6446df118e4adf4f64e66f8
dani_edo <dani.edogawa.17@gmail.com>
2025-03-09 18:57:08 +0700

    outource ReadLines become separated package

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"edo.com/price-calculator/conversion"
+	"edo.com/price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -15,25 +14,10 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
 
+	lines, err := filemanager.ReadLines("prices.txt")
 	if err != nil {
-		fmt.Println("Could not open file!")
 		fmt.Println(err)
-		return
-	}
-
-	scanner := bufio.NewScanner(file)
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println("Reading the content file failed")
-		fmt.Println(err)
-		file.Close()
 		return
 	}
 
@@ -41,12 +25,10 @@ func (job *TaxIncludedPriceJob) LoadData() {
 	floats, err := conversion.StringsToFloats(lines)
 	if err != nil {
 		fmt.Println(err)
-		file.Close()
 		return
 	}
 
 	job.InputPrices = floats
-	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
